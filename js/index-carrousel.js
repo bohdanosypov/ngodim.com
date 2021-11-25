b4e479e7c872c872114e586648c9d01550236806
Bohdan Osypov <gimmeimmortal11@gmail.com>
2021-11-25 03:03:32 +0200

    Fix index html

--- js/index-carrousel.js
+++ js/index-carrousel.js
@@ -10,7 +10,6 @@ function Ant(crslId) {
 		this.crslRoot = document.querySelector('.ant-carousel')
 	};
 
-	// Carousel objects
 	this.crslList = this.crslRoot.querySelector('.ant-carousel-list');
 	this.crslElements = this.crslList.querySelectorAll('.ant-carousel-element');
 	this.crslElemFirst = this.crslList.querySelector('.ant-carousel-element');
@@ -18,22 +17,21 @@ function Ant(crslId) {
 	this.rightArrow = this.crslRoot.querySelector('div.ant-carousel-arrow-right');
 	this.indicatorDots = this.crslRoot.querySelector('div.ant-carousel-dots');
 
-	// Initialization
 	this.options = Ant.defaults;
 	Ant.initialize(this)
 };
 
 Ant.defaults = {
 
-	// Default options for the carousel
-	elemVisible: 3, // Кол-во отображаемых элементов в карусели
-	loop: true,     // Бесконечное зацикливание карусели 
-	auto: true,     // Автоматическая прокрутка
-	interval: 5000, // Интервал между прокруткой элементов (мс)
-	speed: 750,     // Скорость анимации (мс)
-	touch: true,    // Прокрутка  прикосновением
-	arrows: true,   // Прокрутка стрелками
-	dots: true      // Индикаторные точки
+	
+	elemVisible: 3,
+	loop: true,     
+	auto: true,     
+	interval: 5000, 
+	speed: 750,     
+	touch: true,    
+	arrows: true,   
+	dots: true      
 };
 
 Ant.prototype.elemPrev = function(num) {
@@ -44,7 +42,7 @@ Ant.prototype.elemPrev = function(num) {
 	if(this.currentElement < 0) this.currentElement = this.dotsVisible-1;
 	if(this.options.dots) this.dotOff(this.currentElement);
 
-	if(!this.options.loop) {  // сдвиг вправо без цикла
+	if(!this.options.loop) {  
 		this.currentOffset += this.elemWidth*num;
 		this.crslList.style.marginLeft = this.currentOffset + 'px';
 		if(this.currentElement == 0) {
@@ -52,7 +50,7 @@ Ant.prototype.elemPrev = function(num) {
 		}
 		this.rightArrow.style.display = 'block'; this.touchNext = true
 	}
-	else {                    // сдвиг вправо с циклом
+	else {                   
 		let elm, buf, this$ = this;
 		for(let i=0; i<num; i++) {
 			elm = this.crslList.lastElementChild;
@@ -78,7 +76,7 @@ Ant.prototype.elemNext = function(num) {
 	if(this.currentElement >= this.dotsVisible) this.currentElement = 0;
 	if(this.options.dots) this.dotOff(this.currentElement);
 
-	if(!this.options.loop) {  // сдвиг влево без цикла
+	if(!this.options.loop) {  
 		this.currentOffset -= this.elemWidth*num;
 		this.crslList.style.marginLeft = this.currentOffset + 'px';
 		if(this.currentElement == this.dotsVisible-1) {
@@ -86,7 +84,7 @@ Ant.prototype.elemNext = function(num) {
 		}
 		this.leftArrow.style.display = 'block'; this.touchPrev = true
 	}
-	else {                    // сдвиг влево с циклом
+	else {                   
 		let elm, buf, this$ = this;
 		this.crslList.style.cssText = 'transition:margin '+this.options.speed+'ms ease;';
 		this.crslList.style.marginLeft = '-' + this.elemWidth*num + 'px';
@@ -112,20 +110,20 @@ Ant.prototype.dotOff = function(num) {
 
 Ant.initialize = function(that) {
 
-	// Constants
-	that.elemCount = that.crslElements.length; // Количество элементов
-	that.dotsVisible = that.elemCount;         // Число видимых точек
+	
+	that.elemCount = that.crslElements.length;
+	that.dotsVisible = that.elemCount;        
 	let elemStyle = window.getComputedStyle(that.crslElemFirst);
-	that.elemWidth = that.crslElemFirst.offsetWidth +  // Ширина элемента (без margin)
+	that.elemWidth = that.crslElemFirst.offsetWidth +  
 	  parseInt(elemStyle.marginLeft) + parseInt(elemStyle.marginRight);
 
-	// Variables
+	
 	that.currentElement = 0; that.currentOffset = 0;
 	that.touchPrev = true; that.touchNext = true;
 	let xTouch, yTouch, xDiff, yDiff, stTime, mvTime;
 	let bgTime = getTime();
 
-	// Functions
+	
 	function getTime() {
 		return new Date().getTime();
 	};
@@ -138,29 +136,29 @@ Ant.initialize = function(that) {
 		}, that.options.interval)
 	};
 
-	// Start initialization
-	if(that.elemCount <= that.options.elemVisible) {   // Отключить навигацию
+	
+	if(that.elemCount <= that.options.elemVisible) {  
 		that.options.auto = false; that.options.touch = false;
 		that.options.arrows = false; that.options.dots = false;
 		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
 	};
 
-	if(!that.options.loop) {       // если нет цикла - уточнить количество точек
+	if(!that.options.loop) {
 		that.dotsVisible = that.elemCount - that.options.elemVisible + 1;
-		that.leftArrow.style.display = 'none';  // отключить левую стрелку
-		that.touchPrev = false;    // отключить прокрутку прикосновением вправо
-		that.options.auto = false; // отключить автопркрутку
+		that.leftArrow.style.display = 'none';
+		that.touchPrev = false;    
+		that.options.auto = false; 
 	}
-	else if(that.options.auto) {   // инициализация автопрокруки
+	else if(that.options.auto) {  
 		setAutoScroll();
-		// Остановка прокрутки при наведении мыши на элемент
+		
 		that.crslList.addEventListener('mouseenter', function() {
     	clearInterval(that.autoScroll)
     }, false);
 		that.crslList.addEventListener('mouseleave', setAutoScroll, false)
 	};
 
-	if(that.options.touch) {   // инициализация прокрутки прикосновением
+	if(that.options.touch) {  
 		that.crslList.addEventListener('touchstart', function(e) {
 			xTouch = parseInt(e.touches[0].clientX);
 			yTouch = parseInt(e.touches[0].clientY);
@@ -183,7 +181,7 @@ Ant.initialize = function(that) {
 		}, false)
 	};
 
-	if(that.options.arrows) {  // инициализация стрелок
+	if(that.options.arrows) { 
 		if(!that.options.loop) that.crslList.style.cssText =
       'transition:margin '+that.options.speed+'ms ease;';
 		that.leftArrow.addEventListener('click', function() {
@@ -204,14 +202,14 @@ Ant.initialize = function(that) {
     that.rightArrow.style.display = 'none'
 	};
 
-	if(that.options.dots) {  // инициализация индикаторных точек
+	if(that.options.dots) {  
 		let sum = '', diffNum;
 		for(let i=0; i<that.dotsVisible; i++) {
 			sum += '<span class="ant-dot"></span>'
 		};
 		that.indicatorDots.innerHTML = sum;
 		that.indicatorDotsAll = that.crslRoot.querySelectorAll('span.ant-dot');
-		// Назначаем точкам обработчик события 'click'
+		
 		for(let n=0; n<that.dotsVisible; n++) {
 			that.indicatorDotsAll[n].addEventListener('click', function() {
 				diffNum = Math.abs(n - that.currentElement);
@@ -221,10 +219,10 @@ Ant.initialize = function(that) {
 				else if(n > that.currentElement) {
 					bgTime = getTime(); that.elemNext(diffNum)
 				}
-				// Если n == that.currentElement ничего не делаем
+				
 			}, false)
 		};
-		that.dotOff(0);  // точка[0] выключена, остальные включены
+		that.dotOff(0);  
 		for(let i=1; i<that.dotsVisible; i++) {
 			that.dotOn(i)
 		}

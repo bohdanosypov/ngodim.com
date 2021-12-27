1b50f82afc2c34bb6fa6ff8a7ea52a1ecde7d7b5
Bohdan Osypov <gimmeimmortal11@gmail.com>
2021-12-27 19:06:49 +0200

    returned everything to its original form after laboratory work

--- js/index-listeners.js
+++ js/index-listeners.js
@@ -13,34 +13,3 @@ function topFunction() {
 }
 
 
-let effectMouseOverFilter = "grayscale(100%)";
-let effectMouseOutFilter = "grayscale(0)";
-let effectTransition = "0.5s";
-let cardImgHoliday = document.getElementById('card-img-holiday');
-cardImgHoliday.style.filter = effectMouseOverFilter;
-cardImgHoliday.style.transition = effectTransition;
-cardImgHoliday.addEventListener('mouseover', function (e) {
-    cardImgHoliday.style.filter = effectMouseOutFilter;
-});
-cardImgHoliday.addEventListener('mouseout', function (e) {
-    cardImgHoliday.style.filter = effectMouseOverFilter;
-});
-12
-let cardImgCamps = document.getElementById('card-img-camps');
-cardImgCamps.style.filter = effectMouseOverFilter;
-cardImgCamps.style.transition = effectTransition;
-cardImgCamps.addEventListener('mouseover', function (e) {
-   cardImgCamps.style.filter = effectMouseOutFilter;
-});
-cardImgCamps.addEventListener('mouseout', function (e) {
-   cardImgCamps.style.filter = effectMouseOverFilter;
-});
-let cardImgMykolia = document.getElementById('card-img-mykolia');
-cardImgMykolia.style.filter = effectMouseOverFilter;
-cardImgMykolia.style.transition = effectTransition;
-cardImgMykolia.addEventListener('mouseover', function (e) {
-   cardImgMykolia.style.filter = effectMouseOutFilter;
-});
-cardImgMykolia.addEventListener('mouseout', function (e) {
-   cardImgMykolia.style.filter = effectMouseOverFilter;
-});
